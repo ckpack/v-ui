827a3d43a648db085010a856652e80c02700285f
chenkai0520 <k_0520@qq.com>
2022-01-12 17:25:29 +0800

    feat: update type path

--- scripts/vite.es.js
+++ scripts/vite.es.js
@@ -1,24 +1,28 @@
-import {
-  build
-} from 'vite';
 import fs from 'fs';
+import path from 'path';
+import { build } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import dtsPlugin from 'vite-plugin-dts';
 
-const {
-  dependencies
-} = JSON.parse(fs.readFileSync('./package.json'));
+const { dependencies } = JSON.parse(fs.readFileSync('./package.json'));
 const external = ['vue', Object.keys(dependencies ?? [])];
+const rootPath = path.resolve();
+const outDir = 'es';
+const preserveModulesRoot = 'src';
 
 export default build({
   plugins: [dtsPlugin({
-    compilerOptions: {
-      outDir: 'es'
+    cleanVueFileName: true,
+    beforeWriteFile(filePath, content){
+      return {
+        filePath: filePath.replace(`${rootPath}/${outDir}/${preserveModulesRoot}`, `${rootPath}/${outDir}`),
+        content,
+      };
     }
   }), vue()],
   build: {
     target: 'esnext',
-    outDir: 'es',
+    outDir,
     emptyOutDir: true,
     minify: false,
     brotliSize: false,
@@ -27,7 +31,7 @@ export default build({
       output: {
         entryFileNames: '[name].js',
         preserveModules: true,
-        preserveModulesRoot: 'src',
+        preserveModulesRoot,
       },
       external,
     },
