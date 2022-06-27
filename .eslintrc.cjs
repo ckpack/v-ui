872d496f35e392fb9ee6b36b7a990a99afaca509
chenkai0520 <k_0520@qq.com>
2022-06-27 16:53:57 +0800

    chore: 更新依赖

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-reserved-component-names': 0,
     'vue/multi-word-component-names': 0,
     'vue/require-default-prop': 0,
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
