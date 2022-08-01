cedc4b4c0c0f32b3ce5ab6733371d24f8673526e
chenkai0520 <k_0520@qq.com>
2022-08-01 18:32:24 +0800

    style: 测试样式

--- dev/app.ts
+++ dev/app.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
-
 import Dev from './app.vue'
-import '@ckpack/v-ui/src/styles/index.scss'
-const app = createApp(Dev)
+import './index.scss'
 
+const app = createApp(Dev)
 app.mount('#app')
