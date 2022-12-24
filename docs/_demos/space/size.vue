19a6ba0f62e03edb2b7ea7cd5cdcfaf1572466c3
ckvv <k_0520@qq.com>
2022-12-24 16:30:52 +0800

    docs: 更新文档

--- docs/_demos/space/size.vue
+++ docs/_demos/space/size.vue
@@ -1,21 +1,21 @@
 <template>
-  <v-space fill direction="column">
-    <v-space wrap size="small" fill>
+  <v-space direction="column">
+    <v-space wrap size="small">
       <v-button v-for="key in 4" :key="key" theme="primary">
         Button
       </v-button>
     </v-space>
-    <v-space wrap size="medium" fill>
+    <v-space wrap size="medium">
       <v-button v-for="key in 4" :key="key" theme="primary">
         Button
       </v-button>
     </v-space>
-    <v-space wrap size="large" fill>
+    <v-space wrap size="large">
       <v-button v-for="key in 4" :key="key" theme="primary">
         Button
       </v-button>
     </v-space>
-    <v-space wrap size="4rem" fill>
+    <v-space wrap size="4rem">
       <v-button v-for="key in 4" :key="key" theme="primary">
         Button
       </v-button>
