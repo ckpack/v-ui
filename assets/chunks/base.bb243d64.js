412a883b5356a43e52b3803f69d97bb9fbe57c4f
ckvv <ckvv@users.noreply.github.com>
2023-01-29 04:25:50 +0000

    deploy: 8ea8d2c2139819a883b01a95592c1765a7974d35

--- assets/chunks/base.bb243d64.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,o as c,k as _,w as e,b as a,e as s,m as t}from"../app.77d03a2c.js";const m={};function p(d,l){const o=t("v-button"),n=t("v-theme-provider");return c(),_(n,null,{default:e(()=>[a(o,{theme:"primary"},{default:e(()=>[s(" Button ")]),_:1})]),_:1})}const f=r(m,[["render",p]]);export{f as D};
