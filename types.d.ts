0fcf4727d4bd6ab4cbf30e8f90f8bb30589bdf78
ckvv <k_0520@qq.com>
2023-02-14 13:51:21 +0800

    chore: 优化类型

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,8 @@
 // GlobalComponents for Volar
 // Read more: https://github.com/vuejs/core/pull/3399
-
-export declare module '@vue/runtime-core' {
+export declare module 'vue' {
   export type GlobalComponents = typeof import('.')['components']
 }
+
+export {};
 
