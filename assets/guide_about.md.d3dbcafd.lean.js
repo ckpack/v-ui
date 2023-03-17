b39bd8fa7d02212d140fc1544f744ff4402e7b55
ckvv <ckvv@users.noreply.github.com>
2023-03-17 10:30:08 +0000

    deploy: 4fcd86fa2fdbd548b95371b31190a5a447102441

--- assets/guide_about.md.d3dbcafd.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as e,c as a,N as n}from"./chunks/framework.060493b0.js";const h=JSON.parse('{"title":"v-ui","description":"","frontmatter":{},"headers":[],"relativePath":"guide/about.md","lastUpdated":1679031773000}'),o={name:"guide/about.md"},l=n("",13),c=[l];function p(t,i,r,d,C,u){return e(),a("div",null,c)}const D=s(o,[["render",p]]);export{h as __pageData,D as default};
