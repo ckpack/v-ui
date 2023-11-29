ec5e2b30ebd85aaea2ad84660cad1bf59bbc559b
ckvv <ckvv@users.noreply.github.com>
2023-11-29 10:57:51 +0000

    deploy: 6f89e185808169e4f523285a375216c68dd56e0f

---
+++ assets/guide_base_getting-started.md.kDsb3R4S.lean.js
@@ -0,0 +1,5 @@
+import{_ as s,o as i,c as a,R as n,k as t}from"./chunks/framework.Boi7E3CT.js";const F=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"guide/base/getting-started.md","filePath":"guide/base/getting-started.md","lastUpdated":1701255435000}'),p={name:"guide/base/getting-started.md"},h=n("",12),e=t("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"@ckpack/v-ui",src:"https://codepen.io/chenkai0520/embed/wvxKYWj?default-tab=html%2Cresult",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/chenkai0520/pen/wvxKYWj">
+  @ckpack/v-ui</a> by chenkai (<a href="https://codepen.io/chenkai0520">@chenkai0520</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),l=[h,e];function k(E,r,d,c,g,o){return i(),a("div",null,l)}const u=s(p,[["render",k]]);export{F as __pageData,u as default};
